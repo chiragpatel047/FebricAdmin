3942d87b2007c20ff68b2ba50fb0a911e2805a04
chiragpatel047 <chiragpatel4890@gmail.com>
2023-05-02 21:02:12 +0530

    select cat 1

--- app/src/main/java/com/ecomapp/admin/SelectCategories.kt
+++ app/src/main/java/com/ecomapp/admin/SelectCategories.kt
@@ -2,10 +2,94 @@ package com.ecomapp.admin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.ecomapp.admin.Factories.SelectCatVMF
+import com.ecomapp.admin.Models.MainCatModel
+import com.ecomapp.admin.ViewModels.SelectCateViewModel
+import com.ecomapp.admin.databinding.ActivitySelectCategoriesBinding
+import com.ecomapp.febric.Repositories.Response
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint()
 class SelectCategories : AppCompatActivity() {
+
+    lateinit var binding : ActivitySelectCategoriesBinding
+
+    lateinit var selectCateViewModel: SelectCateViewModel
+
+    @Inject
+    lateinit var selectCatVMF: SelectCatVMF
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_select_categories)
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_select_categories)
+        supportActionBar?.hide()
+
+        selectCateViewModel = ViewModelProvider(this,selectCatVMF).get(SelectCateViewModel::class.java)
+
+        val ParentCatSpinnerAdapter =
+            ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, android.R.id.text1)
+
+        ParentCatSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        binding.parentCatSpinner.adapter = ParentCatSpinnerAdapter
+        ParentCatSpinnerAdapter.add("Select category")
+        ParentCatSpinnerAdapter.add("Mens")
+        ParentCatSpinnerAdapter.add("Womens")
+        ParentCatSpinnerAdapter.add("Kids")
+        ParentCatSpinnerAdapter.notifyDataSetChanged()
+
+        val MainCatSpinnerAdapter =
+            ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, android.R.id.text1)
+
+        ParentCatSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        binding.subCatSpinner.adapter = MainCatSpinnerAdapter
+        MainCatSpinnerAdapter.add("Select sub category")
+        MainCatSpinnerAdapter.notifyDataSetChanged()
+
+        selectCateViewModel.mainCat_liveData.observe(this,{
+            when(it){
+                is Response.Sucess -> {
+
+                    MainCatSpinnerAdapter.clear()
+                    MainCatSpinnerAdapter.add("Select sub category")
+
+                    for(singleItem in it.data!!){
+                        MainCatSpinnerAdapter.add(singleItem.mainCatName)
+                    }
+                    MainCatSpinnerAdapter.notifyDataSetChanged()
+                    it.data.clear()
+                }
+                is Response.Error -> {
+
+                }
+                is Response.Loading -> {
+
+                }
+
+            }
+        })
+
+        binding.parentCatSpinner?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                MainCatSpinnerAdapter.clear()
+                MainCatSpinnerAdapter.add("Select sub category")
+                MainCatSpinnerAdapter.notifyDataSetChanged()
+                val selectedBranch = binding.parentCatSpinner.selectedItem.toString()
+                selectCateViewModel.LoadMainCategories(selectedBranch)
+            }
+        }
+
     }
 }
--- app/src/main/java/com/ecomapp/admin/ViewModels/SelectCateViewModel.kt
+++ app/src/main/java/com/ecomapp/admin/ViewModels/SelectCateViewModel.kt
@@ -5,23 +5,35 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ecomapp.admin.Models.MainCatModel
+import com.ecomapp.admin.Models.SubCatModel
 import com.ecomapp.admin.Repositories.DataRepository
 import com.ecomapp.febric.Repositories.Response
 import kotlinx.coroutines.async
 import javax.inject.Inject
 
-class MainCatViewModel @Inject constructor(val dataRepository: DataRepository) : ViewModel() {
+class SelectCateViewModel @Inject constructor(val dataRepository: DataRepository) : ViewModel() {
 
     var mainCat_mutableLiveData = MutableLiveData<Response<ArrayList<MainCatModel>>>()
     val mainCat_liveData : LiveData<Response<ArrayList<MainCatModel>>>
         get() = mainCat_mutableLiveData
 
+    var subCat_mutableLiveData = MutableLiveData<Response<ArrayList<SubCatModel>>>()
+    val subCat_liveData: LiveData<Response<ArrayList<SubCatModel>>>
+        get() = subCat_mutableLiveData
 
     fun LoadMainCategories(catName : String){
         viewModelScope.async {
             val result = dataRepository.LoadMainCategories(catName)
             mainCat_mutableLiveData.postValue(result)
         }
+    }
+
+    fun LoadSubCatigories(parentCatName: String, mainCatName: String) {
+        viewModelScope.async {
+            val result = dataRepository.LoadSubCatigories(parentCatName, mainCatName)
+            subCat_mutableLiveData.postValue(result)
+        }
+
     }
 
 }
